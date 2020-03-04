b8c3ddfa14588f519bfb2094e3724b60758e3060
Jason Scholl <jason.e.scholl@gmail.com>
2020-03-04 06:40:34 -0800

    fix formatting and answers

--- corona.js
+++ corona.js
@@ -2,17 +2,31 @@ var questions = [
   {
     "question": "Did you drink a Corona beer recently?",
     "answers": {
-      "yes": "go drink another beer",
-      "no": "go drink a beer"
+      "yes": "Go drink another beer!",
+      "no": "Go drink a beer."
     }
   },
   {
     "question": "Did you lick a subway pole recently?",
     "answers": {
-      "yes": "what the hell is wrong with you?",
-      "no": "keep up the good hygene"
+      "yes": "What the hell is wrong with you?",
+      "no": "Keep up the good hygene!"
     }
-  }
+  },
+  {
+    "question": "Have you gargled with bleech recently?",
+    "answers": {
+      "yes": "Try a Tidepod chaser.",
+      "no": "That's no way to earn a Darwin Award."
+    }
+  },
+  {
+    "question": "Have you recently been to Italy recently?",
+    "answers": {
+      "yes": "I hope you got some good pictures.",
+      "no": "There's always next year."
+    }
+  },
 ];
 
 questionNum = Math.floor(Math.random() * questions.length);
@@ -22,5 +36,6 @@ function render() {
 }
 
 function answer(response) {
-  document.getElementById("main").innerHTML = questions[questionNum]["answers"][response];
+  diagnosis = [ "Congratulations, you don't have corona virus.  ", "I'm sorry, you have corona virus.  " ][Math.floor(Math.random() * 2)]
+  document.getElementById("main").innerHTML = "<p>" + diagnosis + "</p><p>" + questions[questionNum]["answers"][response] + "</p>";
 }
